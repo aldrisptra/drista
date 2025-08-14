8ed82f776495229e6ca24b25860d2ea381dbd5d1
aldri <11231050@student.itk.ac.id>
2025-08-14 22:31:32 +0800

    update3

--- src/App.jsx
+++ src/App.jsx
@@ -239,7 +239,77 @@ function App() {
       </div>
 
       {/* -------------Pengalaman--------- */}
-
+      <div className="kontak mt-32 sm:p-10 p-0 px-10" id="kontak">
+        <h1
+          className="text-4xl mb-2 font-bold text-center"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          Kontak
+        </h1>
+        <p
+          className="text-base/loose text-center mb-10 opacity-50"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="300"
+        >
+          Mari terhubung dengan saya
+        </p>
+        <form
+          action="https://formsubmit.co/11231050@student.itk.ac.id"
+          method="POST"
+          className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md"
+          autoComplete="off"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="500"
+        >
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <label className="font-semibold">Nama Lengkap</label>
+              <input
+                type="text"
+                name="nama"
+                placeholder="Masukan Nama"
+                className="border border-zinc-500 p-2 rounded-md"
+                required
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label className="font-semibold">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Masukan Email..."
+                className="border border-zinc-500 p-2 rounded-md"
+                required
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="pesan" className="font-semibold">
+                Pesan
+              </label>
+              <textarea
+                name="pesan"
+                id="pesan"
+                cols="45"
+                rows="7"
+                placeholder="Pesan..."
+                className="border border-zinc-500 p-2 rounded-md"
+                required
+              ></textarea>
+            </div>
+            <div className="text-center">
+              <button
+                type="submit"
+                className="bg-teal-700 p-3 rounded-lg w-full cursor-pointer border border-teal-600 hover:bg-teal-600"
+              >
+                Kirim Pesan
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
       {/* -------------kontak----------- */}
     </>
   );
